96c828ade4e665b38724743fa7ab401d545cf2bf
Ken L <klauer@bnl.gov>
2014-06-13 10:45:12 -0400

    Missing motor autosave req files; note about address selection switch

--- ez4axis_example/iocBoot/iocez4axis/st.cmd
+++ ez4axis_example/iocBoot/iocez4axis/st.cmd
@@ -11,9 +11,8 @@ dbLoadDatabase("../../dbd/ez4axis.dbd",0,0)
 ez4axis_registerRecordDeviceDriver(pdbbase) 
 
 epicsEnvSet(P      , "IOC:ALM:")
-epicsEnvSet(EZ_IP  , "192.168.127.254")
-#epicsEnvSet(EZ_IP  , "10.3.0.23")
-epicsEnvSet(EZ_PORT, 4001)
+epicsEnvSet(EZ_IP  , "10.0.0.10")
+epicsEnvSet(EZ_PORT, 4013)
 epicsEnvSet(ASYN_P , "IP1")
 epicsEnvSet(ALM_P  , "ALM1")
 epicsEnvSet(AS     , "AS:")
@@ -26,8 +25,8 @@ dbLoadRecords("$(TOP)/db/example.db","P=$(P),PORT=$(ALM_P),ADDR=0")
 drvAsynIPPortConfigure("$(ASYN_P)", "$(EZ_IP):$(EZ_PORT)", 0, 0, 0)
 
 # almCreateController(AllMotion port name, asyn port name, RS485 address, Number of axes, Moving poll period (ms), Idle poll period (ms))
-#almCreateEZ4Controller("$(ALM_P)", "$(ASYN_P)", 1, 2, 250, 500)
-almCreateEZ4Controller("$(ALM_P)", "$(ASYN_P)", 1, 4, 250, 250)
+almCreateEZ4Controller("$(ALM_P)", "$(ASYN_P)", 2, 4, 100, 250)
+## NOTE: RS485 address must match up with the address selected on the device with its address switch
 
 dbLoadTemplate("motors.sub")
 
